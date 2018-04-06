19543969388be61b1f896fec170658a7d752086b
faraday58 <jarodriguezvera@gmail.com>
2018-04-05 19:55:14 -0500

    Agregando background

--- app/src/main/java/mx/unam/fi/unica/clonspotify/MainActivity.java
+++ app/src/main/java/mx/unam/fi/unica/clonspotify/MainActivity.java
@@ -1,20 +1,27 @@
 package mx.unam.fi.unica.clonspotify;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnIniFacebook;
     Button btnIniSesion;
+    ImageView imgvCon1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgvCon1 = (ImageView)findViewById(R.id.imgvCon1);
+
 
         btnIniFacebook = (Button)findViewById(R.id.btnIniFacebook);
 
